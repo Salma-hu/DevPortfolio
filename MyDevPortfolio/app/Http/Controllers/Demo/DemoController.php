5a2a05c8a5706f45beba105b0285f168e6b18b94
Salma-hu <57940273+Salma-hu@users.noreply.github.com>
2024-09-25 14:49:38 +0100

    Create controller,view about and contact

---
+++ MyDevPortfolio/app/Http/Controllers/Demo/DemoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Demo;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    
+    public function Index(){
+        return view('about');
+    }
+
+    public function ContactMethode(){
+        return view('contact');
+    }
+
+
+
+
+
+}
--- MyDevPortfolio/routes/web.php
+++ MyDevPortfolio/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/about', [DemoController::class,'Index']);
+// Route::get('/about', [DemoController::class, 'Index'])->name('about.page');
+
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'Index');
+    Route::get('/contact', 'ContactMethode')->name('contact.page');
+    // Route::post('/example2', 'method2');
 });
