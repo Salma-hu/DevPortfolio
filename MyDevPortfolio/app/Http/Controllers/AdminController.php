df18f9e737b438d270f34e82d859d59a918d9b33
Salma-hu <57940273+Salma-hu@users.noreply.github.com>
2024-10-17 18:23:29 +0100

    Admin Profile Change Password

--- MyDevPortfolio/app/Http/Controllers/AdminController.php
+++ MyDevPortfolio/app/Http/Controllers/AdminController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -68,7 +69,43 @@ class AdminController extends Controller
         
         return redirect()->route('admin.profile')->with($notification);
         //return redirect()->route('admin.profile');
-    }
+    
+    }//End Method
+
+    public function ChangePassword(){
+
+        return view('admin.admin_change_password');
+
+    }//End Method
+
+    public function UpdatePassword(Request $request){
+
+        $validateData = $request->validate([
+            'oldpassword' => 'required',
+            'newpassword' => 'required',
+            'confirm_password' => 'required|same:newpassword',
+
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+        if (Hash::check($request->oldpassword,$hashedPassword )) {
+            $users = User::find(Auth::id());
+            $users->password = bcrypt($request->newpassword);
+            $users->save();
+
+            session()->flash('message','Password Updated Successfully');
+            return redirect()->back();
+        } else{
+            session()->flash('message','Old password is not match');
+            return redirect()->back();
+        }
+
+    }// End Method
+
+
+
+
+
 
 
 }
