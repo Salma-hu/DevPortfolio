3d5a28bc9f78ebfd7c36b2037d881b7705f3fa03
Salma-hu <57940273+Salma-hu@users.noreply.github.com>
2024-11-08 16:35:52 +0100

    Add Middleware Groups In Route

--- MyDevPortfolio/routes/web.php
+++ MyDevPortfolio/routes/web.php
@@ -29,21 +29,27 @@ use Intervention\Image\Laravel\Facades\Image;
 
 
 Route::controller(DemoController::class)->group(function () {
+
     Route::get('/', 'HomeMain')->name('home');
     Route::get('/about', 'Index')->name('about.page')->middleware('check');
     Route::get('/contact', 'ContactMethode')->name('contact.page');
+
 });
 
 
 // Admin All Route
+Route::middleware(['auth'])->group(function () {
 
-Route::controller(AdminController::class)->group(function () {
-    Route::get('/admin/logout', 'destroy')->name('admin.logout');
-    Route::get('/admin/profile', 'Profile')->name('admin.profile');
-    Route::get('/edit/profile', 'editProfile')->name('edit.profile');
-    Route::post('/store/profile', 'storeProfile')->name('store.profile');
-    Route::get('/change/password', 'ChangePassword')->name('change.password');
-    Route::post('/update/password', 'UpdatePassword')->name('update.password');
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/admin/logout', 'destroy')->name('admin.logout');
+        Route::get('/admin/profile', 'Profile')->name('admin.profile');
+        Route::get('/edit/profile', 'editProfile')->name('edit.profile');
+        Route::get('/change/password', 'ChangePassword')->name('change.password');
+
+        Route::post('/store/profile', 'storeProfile')->name('store.profile');
+        Route::post('/update/password', 'UpdatePassword')->name('update.password');
+    });
+
 });
 
 
@@ -59,19 +65,18 @@ Route::controller(HomeSliderController::class)->group(function () {
 // About Page All Route 
 
 Route::controller(AboutController::class)->group(function () {
-    Route::get('/about/page', 'AboutPage')->name('about.page');
-    Route::post('/update/about', 'UpdateAbout')->name('update.about');
-    Route::get('/about', 'HomeAbout')->name('home.about');
-    
-    Route::get('/about/multi/image', 'AboutMultiImage')->name('about.multi.image');
-    Route::post('/store/multi/image', 'StoreMultiImage')->name('store.multi.image');
 
+    Route::get('/about/page', 'AboutPage')->name('about.page');
+    Route::get('/about', 'HomeAbout')->name('home.about');
+    Route::get('/about/multi/image', 'AboutMultiImage')->name('about.multi.image');
     Route::get('/all/multi/image', 'AllMultiImage')->name('all.multi.image');
     Route::get('/edit/multi/image/{id}', 'EditMultiImage')->name('edit.multi.image');
-    
-    Route::post('/update/multi/image', 'UpdateMultiImage')->name('update.multi.image');
     Route::get('/delete/multi/image/{id}', 'DeleteMultiImage')->name('delete.multi.image');
-     
+    
+    Route::post('/update/about', 'UpdateAbout')->name('update.about');
+    Route::post('/store/multi/image', 'StoreMultiImage')->name('store.multi.image');
+    Route::post('/update/multi/image', 'UpdateMultiImage')->name('update.multi.image');
+
 });
 
 
@@ -80,14 +85,14 @@ Route::controller(AboutController::class)->group(function () {
 Route::controller(PortfolioController::class)->group(function () {
     Route::get('/all/portfolio', 'AllPortfolio')->name('all.portfolio');
     Route::get('/add/portfolio', 'AddPortfolio')->name('add.portfolio');
-    Route::post('/store/portfolio', 'StorePortfolio')->name('store.portfolio');
-
     Route::get('/edit/portfolio/{id}', 'EditPortfolio')->name('edit.portfolio');
-    Route::post('/update/portfolio', 'UpdatePortfolio')->name('update.portfolio');
     Route::get('/delete/portfolio/{id}', 'DeletePortfolio')->name('delete.portfolio');
-    
     Route::get('/portfolio/details/{id}', 'PortfolioDetails')->name('portfolio.details');
     Route::get('/portfolio', 'HomePortfolio')->name('home.portfolio');
+    
+    Route::post('/store/portfolio', 'StorePortfolio')->name('store.portfolio');
+    Route::post('/update/portfolio', 'UpdatePortfolio')->name('update.portfolio');
+    
 });
 
 
